a9cd2c2dccfd35d13c230ebf372c6ec01e085c77
torstein @ server2 <torstein@server2>
2022-11-06 14:13:28 +0100

    add script

--- install.sh
+++ install.sh
@@ -5,6 +5,14 @@ systemd_user_folder="$HOME/.config/systemd/user"
 echo "mkdir for $systemd_user_folder"
 install -v -d -D  $systemd_user_folder
 
+bin="$HOME/bin"
+echo "mkdir for $bin"
+install -vdD  $bin
+
+echo "moving script"
+install start-chromium.sh  $bin
+
+
 echo "moving service and timer"
 
 #  install [OPTION]... [-T] SOURCE DEST
